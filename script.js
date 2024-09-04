ce3539ce3ee8a1ec02bd9522bade3cff7d4fffca
Ashok0077 <vaishnavashok7297@gmail.com>
2024-09-04 20:48:54 +0530

    commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('menuIcon').addEventListener('click', function() {
+    const mobileMenu = document.getElementById('mobileMenu');
+    mobileMenu.classList.toggle('active');
+});
