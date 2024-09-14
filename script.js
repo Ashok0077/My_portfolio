ac2ec5ed53deee4e105780add7f72ca9839ad46a
Ashok0077 <vaishnavashok7297@gmail.com>
2024-09-14 15:21:06 +0530

    optmimize

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ document.getElementById('menuIcon').addEventListener('click', function() {
     const mobileMenu = document.getElementById('mobileMenu');
     mobileMenu.classList.toggle('active');
 });
+
+window.addEventListener('load', () => {
+    const headerImage = new Image();
+    headerImage.src = 'assets/header.webp';
+
+    headerImage.onload = function () {
+        document.body.classList.add('loaded'); 
+    };
+});
